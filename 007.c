d6c4f3def68694a24e4d58c9c64f7d7187a15fed
FLIRER4547 <fernandomarianosilva613@gmail.com>
2025-07-26 13:05:05 -0300

    calculadora melhotada

--- 007.c
+++ 007.c
@@ -1,9 +1,11 @@
 #include <cs50.h>
 #include <math.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void)
 {
+
 
     // Pergunta ao Usuário se ele aceita calcular o IMC.
     char answer = get_char("Você aceita calcular seu IMC?(\033[1;32my\033[0m ou \033[1;31mn\033[0m): ");
@@ -12,29 +14,52 @@ int main(void)
     if (answer == 'y' || answer == 'Y')
     {
 
-        // Armazena os dados em variáveis e faz o calculo do IMC.
-        float p = get_float("Qual o seu peso em KG? ");
-        float al = get_float("Qual sua altura em metros? ");
-        float imc = p / pow(al, 2);
-
-        // Condicionais relacionadas ao resultado do IMC.
-        if (imc < 18.5)
+        // Faz o programa funcionar 'eternamente'.
+        while (true)
         {
 
-            // IMC menor que 18.5, significa peso abaixo do recomendado.
-            printf("O seu IMC é %.2f, você está \033[1;31mabaixo\033[0m do peso recomendado.\n", imc);
-        }
-        else if (imc >= 18.5 && imc <= 24.9)
-        {
+            // Armazena os dados em variáveis e faz o calculo do IMC.
+            float p = get_float("Qual o seu peso em KG? ");
+            float al = get_float("Qual sua altura em metros? ");
+            float imc = p / pow(al, 2);
 
-            // Se for entre 18.5 e 24.9, está bom.
-        printf("\033[1;32mSeu IMC é %.2f, você está com o peso recomendado, meus parabens!\033[0m\n", imc);
-        }
-        else
-        {
+            // Condicionais relacionadas ao resultado do IMC.
+            if (imc < 18.5)
+            {
 
-            // IMC maior que 24.9, significa sobrepeso.
-            printf("O seu IMC é %.2f, você está \033[1;31macima\033[0m do peso recomendado.\n", imc);
+                // IMC menor que 18.5, significa peso abaixo do recomendado.
+                printf("O seu IMC é %.2f, você está \033[1;31mabaixo\033[0m do peso recomendado.\n", imc);
+            }
+            else if (imc >= 18.5 && imc <= 24.9)
+            {
+
+                // Se for entre 18.5 e 24.9, está bom.
+            printf("\033[1;32mSeu IMC é %.2f, você está com o peso recomendado, meus parabens!\033[0m\n", imc);
+            }
+            else
+            {
+
+                // IMC maior que 24.9, significa sobrepeso.
+                printf("O seu IMC é %.2f, você está \033[1;31macima\033[0m do peso recomendado.\n", imc);
+            }
+
+            // Pergunta se o programa será reiniciado.
+            char reiniciar = get_char("\033[1;33mDigite 'R' para reiniciar ou 'E' para encerrar a calculadora:\033[0m ");
+
+            // Reinicia o Programa.
+            if (reiniciar == 'r' || reiniciar == 'R' )
+            {
+            printf("\033[1;32mReiniciando...\033[0m\n");
+            sleep(2);
+            }
+
+            // Encerra o programa.
+            else
+            {
+                printf("\033[1;31mEncerrando...\033[0m\n");
+                sleep(2);
+                break;
+            }
         }
     }
 
